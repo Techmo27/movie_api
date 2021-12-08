42386780884d30e04eddfb3120ed8e5c8715338b
Techmo27 <e.vins@yahoo.com>
2021-12-08 22:24:23 +0100

    Added Endpoints To API
    Added Code to Route All Endpoints
    
    Naming Change name to TITLE
    
    Colon Update Favorites
    
    Changed Endpoints for favorites
    
    Typo Correction Routing
    
    Changed Endpoints
    
    Changed Endpoints
    
    Changed Endpoint
    
    Changed Endpoints
    
    changed endpoint
    
    changed endpoints
    
    Updated Documentation
    
    UUID + Body Parser Node Modules Gitignore
    
    Added Movies To In-Memory
    
    Removed Typo

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
 const app = express();
-let topMovies = [];
 
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({
-  extend:true
- }
-));
-app.use(bodyParser.json());
+let topMovies = [
+  {
+    title: 'Movie1'
+  },
+  {
+    title: 'Movie2'
+  },
+  {
+    title: 'Movie3'
+  }
+];
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('This is my movie api!');
+  res.send('Welcome to the myFlix app!');
 });
 
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+app.get('/movies/:title', (req, res) => {
+  res.send('Successful GET request which shows a movie by its title.');
+});
+
+app.get('/genres/:genre', (req, res) => {
+  res.send('Successful GET request which describes the genre of a movie.');
+});
+
+app.get('/directors/:name', (req, res) => {
+  res.send('Successful GET request which introduces the director.');
+});
+
+app.post('/users', (req, res) => {
+  res.send('User has been added successfully!');
+});
+
+app.put('/users/:username', (req, res) => {
+  res.send('User has successfully changed her/his username!');
+});
+
+app.get('/users/:username/favorites', (req, res) => {
+  res.send('Successful GET request that returns a list of users favorites.');
+});
+
+app.post('/users/:username/favorites/:movieID', (req, res) => {
+  res.send('User added one movie to her/his favorites list.');
+});
+
+app.delete('/users/:username/favorites/:movieID', (req, res) => {
+  res.send('User has successfully deleted one movie from her/his favorites.');
+});
+
+app.delete('/users/:username', (req, res) => {
+  res.send('Users account has been deleted!');
 });
 
 app.use((err, req, res, next) => {
