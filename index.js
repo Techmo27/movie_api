d74bc552e07a36f40cdb33b584760a443925683c
Techmo27 <e.vins@yahoo.com>
2021-12-10 11:12:47 +0100

    Changed Endpoints for favorites

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ app.put('/users/:username', (req, res) => {
   res.send('User has successfully changed her/his username!');
 });
 
-app.post('users/:username/:favorites/:ID', (req, res) => {
+app.post('users/:username/movie/:movieID', (req, res) => {
   res.send('User added one movie to her/his favorites list.');
 });
 
-app.delete('users/:username/favorites/:ID', (req, res) => {
+app.delete('users/:username/movie/:movieID', (req, res) => {
   res.send('User has successfully deleted one movie from her/his favorites.');
 });
 
