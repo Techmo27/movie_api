2a817a5bafb36f7a894f3c9f1bf6e23bdc153ca8
Techmo27 <e.vins@yahoo.com>
2022-01-08 22:15:47 +0100

    Deleted CORS Origin Restriction Code

--- index.js
+++ index.js
@@ -19,21 +19,11 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-const cors = require('cors');
-
 // restricts the access to only certain origins
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+const cors = require('cors');
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');
